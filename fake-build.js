dc78642d521f9cc46dbffeceacc4fb30fd0e12df
Dmitriy Davydov <dvdvdmt@gmail.com>
2020-03-16 08:15:39 +0500

    add build error probability

--- fake-build.js
+++ fake-build.js
@@ -5,13 +5,21 @@ const interval = Math.floor(milliSecondsToRun / numberOfLogMessages);
 postLogMessageOrExit();
 
 function postLogMessageOrExit(timePassed=0, messageCounter=1) {
-  if (timePassed < milliSecondsToRun) {
+  if(buildMustFail()){
+    console.log('❌ Build failed.');
+    process.exit(1);
+  } else if (timePassed < milliSecondsToRun) {
     console.log(`Log message #${messageCounter}`);
     setTimeout(() => {
       postLogMessageOrExit(timePassed + interval, messageCounter + 1);
     }, interval);
   } else {
-    console.log('Build finished.');
+    console.log('✅ Build finished.');
     process.exit(0);
   }
+}
+
+function buildMustFail() {
+  const errorProbability = 0.05;
+  return errorProbability >= Math.random();
 }
